d69be1ff11f4cdc2283a252f37959f65335e1ab1
tronku <shubham.pathak2000@gmail.com>
2019-02-06 16:27:23 +0530

    Add another activity

--- app/src/main/java/project/tronku/workshop/MainActivity.java
+++ app/src/main/java/project/tronku/workshop/MainActivity.java
@@ -2,12 +2,25 @@ package project.tronku.workshop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button playButton; //can make it local too
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playButton = findViewById(R.id.play_button);
+
+        //Listener for the Play Button
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //to be filled....
+            }
+        });
     }
 }
